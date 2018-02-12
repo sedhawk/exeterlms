e7dfc475c5295b794d8cc6d4d75233fef8cd2716
unknown <speerdw@gmail.com>
2018-02-11 19:48:30 -0700

    regedit complete

---
+++ FSW101/Lesson4/Lesson4.js
@@ -0,0 +1,38 @@
+/*********************************************************************************
+ * Don Speer
+ * Course: FSWO101 Lesson 4
+ * Date: February 10, 2018
+ * Purpose: Hands-On: Decision Making
+*********************************************************************************/
+
+// PART 1
+// Converting if...else into a ternary
+function phoneNumberFormat(phoneNumber) {
+    phoneNumber = document.getElementById("phNumber").value;
+    let phoneRegex = /^\(\d\d\d\) \d\d\d-\d\d\d\d$/g;
+    phoneNumber.match(phoneRegex) ? alert("Valid number entered") : alert("Phone number entered is invalid");
+}
+
+//PART 2
+// try...catch
+let newNumber = 10;
+try {
+    if (newNumber < 5) throw true;
+} catch {
+    console.log("Error: Invalid Number");
+}
+
+// PART 3
+// Checks the input of a user's name for the proper format
+function checkName(checkName){
+    checkName = document.getElementById("name").value;
+    let nameRegex = /^[A-Z][a-z]+ [A-Z][a-z]+/g
+    try {
+        if (checkName.match(nameRegex)) alert("Valid name entered");
+        console.log(true);
+    }
+    catch {
+        alert("Name entered is invalid");
+        console.log(false);
+    }
+}
