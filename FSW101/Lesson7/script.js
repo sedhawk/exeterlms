e5cb56174d4b529fd8c3291254136b19c50981e1
unknown <speerdw@gmail.com>
2018-02-13 20:25:35 -0700

    ajax and jquery

---
+++ FSW101/Lesson7/script.js
@@ -0,0 +1,20 @@
+/*function loadDoc(url, callback) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        callback(this);
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }
+  
+  function myFunction(xhttp) {
+    document.getElementById("example").innerHTML = xhttp.responseText;
+  }*/
+
+  $(document).ready(function() {
+    $("#driver").click(function(event) {
+      $("#stage").load("info.html");
+    });
+  });
