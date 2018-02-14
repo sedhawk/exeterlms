897020ca06963b6f230303b9d8ce0e902d96c18e
sedhawk <speerdw@gmail.com>
2018-02-13 17:15:50 -0700

    Final Project to fix

--- FSW101/Lesson6/HandsOnPart1/script.js
+++ FSW101/Lesson6/HandsOnPart1/script.js
@@ -5,16 +5,32 @@
  * Date: February 13, 2018
  *\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
 
-// PART 1
-// uses dblclick() to change the CSS color to blue on the h1 tag. The other will change the font size on hover
+// PART 1, 2, and 3
+// Uses dblclick() to change the CSS color to blue on the h1 tag 
+// The other will change the font size on hover
+// Create a web page that has at least six different elements that are changed 
+// with jQuery when you either click, hover, keyup, dblclick, font color,
+// font size, background color, font type
 $(document).ready(function(){    
-    $("h1").dblclick(function(){
+   $("#firstDiv").hover(function(){
+       $(this).css("background-color", "green");
+   }, function(){
+       $(this).css("background-color", "blue");
+   });
+
+   $("#secondDiv").hover(function(){
+       $(this).css("background-color", "green");
+   }, function(){
+       $(this).css("background-color", "blue");
+   });
+
+   $("h1").dblclick(function(){
 		$(this).css("color", "blue");
  	});
- });
 
- $(document).ready(function(){
     $("h1").hover(function(){
         $(this).css("font-size", '72px');
     });
+
+
 });
