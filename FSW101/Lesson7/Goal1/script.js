baee16602ab83e398182ea02bb295f43ebfea25f
unknown <speerdw@gmail.com>
2018-02-23 14:50:04 -0700

    lesson7 fixes

--- FSW101/Lesson7/Goal1/script.js
+++ FSW101/Lesson7/Goal1/script.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $("#driver").click(function(event) {
       $("#stage").load("info.html");
     });
+    $('.toggle').click(function(e){
+      e.preventDefault();
+    $("#driver1").attr('src', "../../../images/Einstein.jpeg");
   });
+});
