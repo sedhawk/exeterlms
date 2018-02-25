1061a2877e40beb92f4cc2e9d3ede8238a47f123
unknown <speerdw@gmail.com>
2018-02-24 22:41:25 -0700

    final touches

--- FSW101/Lesson7/Goal1/script.js
+++ FSW101/Lesson7/Goal1/script.js
@@ -1,6 +1,10 @@
+
 $(document).ready(function() {
     $("#driver").click(function(event) {
       $("#stage").load("info.html");
+    });
+    $("#driver").click(function(event) {
+      $("#stage1").load("footer.html");
     });
     $("#driver2").click(function(e){
       e.preventDefault();
