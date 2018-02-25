81c4be6c376ed3bc491496ac8b8cb2081c0b4c70
unknown <speerdw@gmail.com>
2018-02-24 21:40:30 -0700

    Lesson7 final touches

--- FSW101/Lesson7/Goal1/script.js
+++ FSW101/Lesson7/Goal1/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#driver").click(function(event) {
       $("#stage").load("info.html");
     });
-    $('.toggle').click(function(e){
+    $("#driver2").click(function(e){
       e.preventDefault();
     $("#driver1").attr('src', "../../../images/albert-einstein.jpg");
   });
