b4111673f47b359d45a101f5388715359089c008
unknown <speerdw@gmail.com>
2018-02-25 23:22:54 -0700

    more stuff

--- FSW101/Lesson7/Goal1/script.js
+++ FSW101/Lesson7/Goal1/script.js
@@ -1,13 +1,30 @@
+var data = [
+  {
+  src: '../../../images/Einstein.jpeg',
+  text: "quote1.html",
+  footer: "footer1.html",
+  header: "Albert Einstein"
+  },
+  {
+  src: '../../../images/albert-einstein.jpg',
+  text: "quote2.html",
+  footer: "footer2.html",
+  header: "Albert Einstein In Color"
+  },
+];
+
+var randomIndex = Math.floor(Math.random() * data.length);
+var randomData = data[randomIndex];
 
 $(document).ready(function() {
     $("#driver").click(function(event) {
-      $("#stage").load("info.html");
+      $("#stage").load("quote1.html");
     });
     $("#driver").click(function(event) {
-      $("#stage1").load("footer.html");
+      $("#stage1").load("footer1.html");
     });
     $("#driver2").click(function(e){
       e.preventDefault();
-    $("#driver1").attr('src', "../../../images/albert-einstein.jpg");
+    $("#driver1").attr('src', randomData.src);
   });
 });
