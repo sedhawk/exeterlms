a478f4b27d1326be9a066df638d80cc4e477ae01
unknown <speerdw@gmail.com>
2018-03-03 22:55:05 -0700

    educational pictures

--- FSW101/Final/js/main.js
+++ FSW101/Final/js/main.js
@@ -52,15 +52,15 @@ $(function(){
 // Progress Bar
 // -------------------------------------------------------------
 
-(function () {
+$(document).ready(function () {
 
-    $('.progress-content').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-        if (visible) {
+   /* $('.progress-content').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {*/
             $.each($('div.progress-bar'),function(){
                 $(this).css('width', $(this).attr('aria-valuenow')+'%');
             });
-            $(this).unbind('inview');
-        }
+            //$(this).unbind('inview');
+        //}
     });
     $('.rating-bar').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
         if (visible) {
