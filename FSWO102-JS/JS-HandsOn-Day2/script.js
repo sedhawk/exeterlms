ecbdb64039f6edaad8be4f53c3cab2d7ef29aea5
sedhawk <sedhawk@ITS-FENEGD4S9JG.egr.nau.edu>
2018-04-16 22:28:48 -0700

    bing bong

---
+++ FSWO102-JS/JS-HandsOn-Day2/script.js
@@ -0,0 +1,13 @@
+function bingBong (){
+
+  let result = "";
+
+    for(let i=0; i<100; i++){
+      if (i % 3 == 0) result = "Bing";
+      else if (i % 5 == 0) result = "Bong"
+      else if (i%3 == 0 && i%5 == 0) result = "Bing Bong";
+      else result = i;
+      document.getElementById("output").innerHTML += result + '\n';
+      i++;
+    }
+  }
