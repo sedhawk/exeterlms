8e9b4cde0ade5c453b0e65875da1a8e5b82ccbdd
sedhawk <sedhawk@ITS-FENEGD4S9JG.egr.nau.edu>
2018-04-16 22:46:29 -0700

    bug fix

--- FSWO102-JS/JS-HandsOn-Day2/script.js
+++ FSWO102-JS/JS-HandsOn-Day2/script.js
@@ -3,9 +3,9 @@ function bingBong (){
   let result = "";
 
     for(let i=0; i<100; i++){
-      if (i % 3 == 0) result = "Bing";
-      else if (i % 5 == 0) result = "Bong"
-      else if (i%3 == 0 && i%5 == 0) result = "Bing Bong";
+      if (i%3 == 0 && i%5 == 0) result = "Bing Bong";
+      else if (i % 3 == 0) result = "Bing"
+      else if (i % 5 == 0) result = "Bong";
       else result = i;
       document.getElementById("output").innerHTML += result + ' \n';
       i++;
