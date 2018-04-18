5b29dcd75d5a1982985885ef950fb6405c7ca116
sedhawk <sedhawk@ITS-FENEGD4S9JG.egr.nau.edu>
2018-04-17 20:36:51 -0700

    classes

---
+++ FSWO102-JS/JS-HandsOn-Day3/script.js
@@ -0,0 +1,37 @@
+//In this project, we will be building a Family Tree.
+//You will be creating a class for grandparents that includes properties:
+//Hair Color, Eye Color, and Vertical. Hair Color and Eye Color can be strings,
+//but Vertical should be a method that display how high the person can jump in
+//the console.
+/*
+You will need at least three generations: Grandparents, Parents, and Children
+This should be built using classes for the Grandparents, each following
+Generation should inherent from the generation before them all the up to
+grandparents.
+Your objects should be instantiate and added to an array.
+Loop through said array and console log each individuals vertical in the
+console. Ex: "Grandma can jump 22 inches."
+*/
+class Grandparents {
+  set hairColor(hair){
+    this._hairColor = hair;
+  }
+  get hairColor(){
+    return this._hairColor;
+  }
+  set eyeColor(eyes){
+    this._eyeColor = eyes;
+  }
+  get eyeColor(eyes){
+    return this._eyeColor;
+  }
+  set vertical(jump){
+    this._vertical = jump;
+  }
+  get vertical(){
+    return console.log("Grandpa can jump " + this._vertical + " inches");
+  }
+}
+
+let grandpa = new Grandparents();
+grandpa.vertical(22);
