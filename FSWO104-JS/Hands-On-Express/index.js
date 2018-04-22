54f3c5dad05bc8117e010734738c9ec9f7e7986d
sedhawk <sedhawk@ITS-FENEGD4S9JG.egr.nau.edu>
2018-04-22 14:38:53 -0700

    added hyperlinks

--- FSWO104-JS/Hands-On-Express/index.js
+++ FSWO104-JS/Hands-On-Express/index.js
@@ -7,11 +7,19 @@ app.set('view engine', 'handlebars');
 app.set("port", process.env.PORT || 3000);
 
 app.get('/', (request, response) => {
-    response.render('home');
+    response.render('beginning');
 });
 
-app.get('about', (request, response) => {
-    response.render('about');
+app.get('middle', (request, response) => {
+    response.render('middle');
+});
+
+app.get('end', (request, response) => {
+    response.render('end');
+});
+
+app.get('final', (request, response) => {
+    response.render('final');
 });
 
 app.use((request, response) => {
