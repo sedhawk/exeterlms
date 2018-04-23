007e631a2982c629da79b344a994d184f5e02ad3
sedhawk <sedhawk@ITS-FENEGD4S9JG.egr.nau.edu>
2018-04-22 22:06:51 -0700

    Persistence Sqlite

---
+++ FSWO104-JS/Persistence/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const sqlite = require("./node_modules/sqlite").verbose();
+
+const db = new sqlite3.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");
+
+db.each(query, (err, row) => {
+  if (err) throw err;
+  console.log(row);
+});
+
+db.close();
