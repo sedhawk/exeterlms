e070f2fbcf70a7cebaa1a80f42cd5ed22ff07a35
sedhawk <sedhawk@ITS-FENEGD4S9JG.egr.nau.edu>
2018-04-22 22:41:49 -0700

    more instructions

--- FSWO104-JS/Persistence/index.js
+++ FSWO104-JS/Persistence/index.js
@@ -3,9 +3,17 @@ const sqlite = require("sqlite3").verbose();
 
 const db = new sqlite.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");
 
+/*
 db.each(`SELECT * from Artist LIMIT 100`, (err, row) => {
   if (err) throw err;
   console.log(row);
+});
+*/
+app.post('/form', (req, res) => {
+  db.run(`INSERT into Artist(ArtistId, Name) VALUES(231, Lil Wayne)`, (err, row) => {
+    if (err) throw err;
+    res.redirect(303, '/success');
+  });
 });
 
 db.close();
