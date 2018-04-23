fcb55642737cdb462551b4f9287d0e9338e09bf7
sedhawk <sedhawk@ITS-FENEGD4S9JG.egr.nau.edu>
2018-04-22 22:37:30 -0700

    fixed sqlite database

--- FSWO104-JS/Persistence/index.js
+++ FSWO104-JS/Persistence/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const sqlite = require("sqlite").verbose();
+const sqlite = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");
+const db = new sqlite.Database("./Chinook_Sqlite_AutoIncrementPKs.sqlite");
 
-db.each(query, (err, row) => {
+db.each(`SELECT * from Artist LIMIT 100`, (err, row) => {
   if (err) throw err;
   console.log(row);
 });
