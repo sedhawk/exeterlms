d95f54c855f3ae83f640defc032c8ef80a22ef2e
sedhawk <sedhawk@ITS-FENEGD4S9JG.egr.nau.edu>
2018-04-23 22:47:14 -0700

    CRUD Stuff

--- FSWO104-JS/SimpleServer/index.js
+++ FSWO104-JS/SimpleServer/index.js
@@ -22,3 +22,32 @@ app.put("/", function(req, res) {
 app.delete("/", function(req, res) {
   res.send("You successfully created a DELETE route!");
 });
+
+// Initiating functionality
+
+app.use(require("body-parser")());
+
+/*  Generic way to do this
+app.post('/fullstory', (req, res) => {
+  res.send("This is " + req.body.name);
+});
+*/
+// Ajax way
+app.post('/fullstory', (req, res) => {
+  
+    if(req.xhr || req.accepts('json,html')==='json'){
+        // handle errors if any
+        res.send({ success: true });
+    } else {
+        // handle errors if any
+        res.redirect(303, '/success');
+    }
+});
+
+app.get("/form", function(req, res) {
+  res.sendfile('views/form.html');
+});
+
+app.get("/success", function(req, res) {
+  res.sendfile('views/success.html');
+});
