e81972da6e8d7f3f557bfe543b12e3b1f0d36226
sedhawk <speerdw@gmail.com>
2018-05-03 14:10:23 -0700

    rename

--- JS-Basics/BingBong/script.js
+++ JS-Basics/BingBong/script.js
@@ -4,7 +4,7 @@ function bingBong (){
 
     for(let i=0; i<100; i++){
       if (i%3 == 0 && i%5 == 0) result = "Bing Bong";
-      else if (i % 3 == 0) result = "Bing"
+      else if (i % 3 == 0) result = "Bing";
       else if (i % 5 == 0) result = "Bong";
       else result = i;
       document.getElementById("output").innerHTML += result + ' \n';
